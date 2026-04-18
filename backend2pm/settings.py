94949f45a183c1ed7153f055f16e3e33ebe49965
Pranav <pranavgamerz43@gmail.com>
2026-04-19 00:39:53 +0545

    Fix Django import/static processor + psycopg version for local dev - BLACKBOXAI

--- backend2pm/settings.py
+++ backend2pm/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'django.contrib.staticfiles.context_processors.static',  # Disabled - Render import error; not required
+'django.template.context_processors.static',
             ],
         },
     },
